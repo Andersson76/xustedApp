faca51df2349d101f6b22b1070116fcefe621f86
Martin Andersson <martin.andersson@me.com>
2024-08-13 13:35:12 +0200

    Adding JSON data to github for fetching

---
+++ XustedMusicApp/screens/HomeScreen.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+  Image
+} from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  const [albums, setAlbums] = useState([]);
+
+useEffect(() => {
+  fetch(
+    "https://raw.githubusercontent.com/Andersson76/xustedApp/main/assets/data/albums.json"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data); // Kontrollera här att du får vad du förväntar dig
+      setAlbums(data.albums);
+    })
+    .catch((error) => console.error("Fetch error:", error));
+}, []);
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={albums}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Album", { album: item })}
+          >
+            <View style={styles.albumContainer}>
+              <Image source={{ uri: item.cover }} style={styles.coverImage} />
+              <Text style={styles.albumTitle}>{item.title}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  albumContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  coverImage: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  albumTitle: {
+    fontSize: 18,
+  },
+});
